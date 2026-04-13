a7205956494f88fbbcacbf858a894da440c97a62
s-blanco-dev <santiago.blancoc@correo.ucu.edu.uy>
2026-04-13 18:00:31 -0300

    marcha joyazo

--- components/delay/include/delay.h
+++ components/delay/include/delay.h
@@ -1,4 +1,9 @@
 #include <stdint.h>
+#ifndef DELAY_H
+#define DELAY_H
+
 
 void delay_ms(uint32_t ms);
 void delay_s(uint32_t s);
+
+#endif // !DELAY_H
--- main/laboratorio.c
+++ main/laboratorio.c
@@ -1,7 +1,10 @@
 #include "../components/delay/include/delay.h"
+#include "esp_err.h"
 #include "esp_log.h"
 #include "esp_log_level.h"
+#include "led_strip.h"
 #include "sdkconfig.h"
+#include <stdint.h>
 #include <stdio.h>
 
 #define ARRAY_SIZE 12
@@ -9,12 +12,31 @@
 int exampleData;
 char exampleArray[ARRAY_SIZE];
 
+led_strip_t *led;
+
 void app_main(void) {
   // probemos
-  int ing = 0;
-  while (true) {
-    ing++;
-    ESP_LOGI("", "ho ho ho: %d\n", ing);
-    delay_s(2);
+  // int ing = 0;
+  // while (true) {
+  //   ing++;
+  //   ESP_LOGI("", "ho ho ho: %d\n", ing);
+  //   delay_s(2);
+  // }
+
+  if (led_rgb_init(&led) != ESP_OK) {
+    ESP_LOGE("Ojaldre", "Reventó Fermat\n");
+  }
+
+  uint8_t r, g, b = 0;
+  r = 255;
+  g = 0;
+
+  while (r != g) {
+    r--;
+    g++;
+    b++;
+    led->set_pixel(led, 0, r, g, b);
+    led->refresh(led, 100);
+    delay_ms(100);
   }
 }
